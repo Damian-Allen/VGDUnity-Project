31e5690f7964b00498d8e45a20f7b72f963a1346
Damian <damian.allen@easternstudent.ca>
2021-03-10 11:44:08 -0400

    no message

---
+++ ShapeMon/Assets/Scripts/GameController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class GameController : MonoBehaviour {
+    private static GameController _instance;
+
+    public static GameController Instance { get { return _instance; } }
+
+
+    private void Awake() {
+        if (_instance != null && _instance != this) {
+            Destroy(this.gameObject);
+        } else {
+            _instance = this;
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Start adding from here
+
+    public Sprite[] shapeMon;
+
+    [Tooltip("% chance for an enconter")]
+    public int encounterChance;
+    public int i;
+    public TextMeshProUGUI test;
+
+
+    public void EncounterCheck() {
+
+        if (encounterChance == 0) {
+            encounterChance = 10;
+        }
+        
+        i = Random.Range(1, 101);
+        if (i <= encounterChance){
+            SceneManager.LoadScene(1);
+        }
+
+    }
+
+    public void Button() {
+        SceneManager.LoadScene(0);
+    }
+
+
+
+
+}
---
+++ ShapeMon/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class PlayerController : MonoBehaviour {
+
+    enum Direction {
+        UP,LEFT,RIGHT,DOWN,NONE
+    }
+    
+    private Direction currentDir;
+    private Vector3 targetTransform;
+    private bool amMove;
+    private bool running = false;
+
+    [Tooltip("Lower Values for faster movement. DO NOT GO TO 0!")]
+    public float timeToMove;
+    public Tilemap wallTileMap;
+    public Tilemap encounterTileMap;
+    private TileBase checkWallTile;
+    private TileBase checkEncounterTile;
+
+    
+
+    void Start() {
+
+    }
+
+    void FixedUpdate() {
+
+        if(!amMove){
+            GetInput();
+        }
+
+        GetDirection();
+
+        
+
+        if (checkWallTile == null) {
+            if (amMove && !running) {
+                running = true;
+                StartCoroutine(LerpPosition(targetTransform, timeToMove));
+            }
+        } else {
+            amMove = false;
+        }
+        
+        
+        
+    }
+
+    private void GetInput() {
+        if(Input.GetAxis("Vertical") > 0 ){
+                currentDir = Direction.UP;
+                amMove = true;
+            } else if(Input.GetAxis("Vertical") < 0 ){
+                currentDir = Direction.DOWN;
+                amMove = true;
+            } else if(Input.GetAxis("Horizontal") > 0){
+                currentDir = Direction.RIGHT;
+                amMove = true;
+            } else if(Input.GetAxis("Horizontal") < 0){
+                currentDir = Direction.LEFT;
+                amMove = true;
+            }
+    }
+
+    private void GetDirection() {
+        switch (currentDir){
+            case Direction.UP: 
+                targetTransform = transform.position + Vector3.up;
+                checkWallTile = wallTileMap.GetTile(Vector3Int.FloorToInt(targetTransform));
+                checkEncounterTile = encounterTileMap.GetTile(Vector3Int.FloorToInt(targetTransform));
+                currentDir = Direction.NONE;
+                break;
+
+            case Direction.DOWN:
+                targetTransform = transform.position + Vector3.down;
+                checkWallTile = wallTileMap.GetTile(Vector3Int.FloorToInt(targetTransform));
+                checkEncounterTile = encounterTileMap.GetTile(Vector3Int.FloorToInt(targetTransform));
+                currentDir = Direction.NONE;
+                break;
+
+            case Direction.LEFT:
+                targetTransform = transform.position + Vector3.left;
+                checkWallTile = wallTileMap.GetTile(Vector3Int.FloorToInt(targetTransform));
+                checkEncounterTile = encounterTileMap.GetTile(Vector3Int.FloorToInt(targetTransform));
+                currentDir = Direction.NONE;
+                break;
+
+            case Direction.RIGHT:
+                targetTransform = transform.position + Vector3.right;
+                checkWallTile = wallTileMap.GetTile(Vector3Int.FloorToInt(targetTransform));
+                checkEncounterTile = encounterTileMap.GetTile(Vector3Int.FloorToInt(targetTransform));
+                currentDir = Direction.NONE;
+                break;
+
+            default: break;
+        }
+    }
+
+    IEnumerator LerpPosition(Vector3 targetPosition, float duration) {
+        float time = 0;
+        Vector3 startPosition = transform.position;
+
+        while (time < duration) {
+            transform.position = Vector2.Lerp(startPosition, targetPosition, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        if (checkEncounterTile != null) {
+            GameController.Instance.EncounterCheck();
+        }
+        transform.position = targetPosition;
+        amMove = false;
+        running = false;
+    }
+
+
+
+}
